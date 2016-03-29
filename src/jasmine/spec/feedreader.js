80b99fd4594f9dfd1bacccc3906af4d2a35984bc
developer2020 <dev276236@gmail.com>
2016-03-29 09:16:06 +0400

    completed test suite 'The Menu'

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -10,9 +10,9 @@
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -32,10 +32,10 @@ $(function() {
          * and that the URL is not empty.
          */
         it('each  feed URL is defined and not empty', function() {
-            for (var i = 0, len = allFeeds.length; i<len; i++){
-            expect(allFeeds[i].url).toBeDefined();
-            expect(allFeeds[i].url).not.toBe("");
-        }
+            for (var i = 0, len = allFeeds.length; i < len; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe("");
+            }
         });
 
 
@@ -44,10 +44,10 @@ $(function() {
          * and that the name is not empty.
          */
         it('each  feed name is defined and not empty', function() {
-            for (var i = 0, len = allFeeds.length; i<len; i++){
-            expect(allFeeds[i].name).toBeDefined();
-            expect(allFeeds[i].name).not.toBe("");
-        }
+            for (var i = 0, len = allFeeds.length; i < len; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe("");
+            }
         });
 
     });
@@ -56,27 +56,37 @@ $(function() {
     /* This test suite tests the menu*/
     describe('The Menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This test  checks that the menu element is
+         * hidden by default. 
          */
+        it('menu is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+
+        /* This test ensures that the menu changes
+         * visibility when the menu icon is clicked. It 
+         * has two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('menu changes visibility when clicked', function() {
+            var menuIcon = $('.menu-icon-link');
+            menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
@@ -85,3 +95,4 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 }());
+
